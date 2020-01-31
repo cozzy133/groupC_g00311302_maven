173402f458d56ce3fee584f70975f845ce296160
g00311302 <g00311302@gmit.ie>
2020-01-31 13:18:01 +0000

    IntelliJ update #3

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -3,8 +3,7 @@ package ie.gmit;
 // Adding in new information
 public class Calculator {
 
-    public double add(double num1, double num2) { // This method will add two numbers
+    public double add(double num1, double num2) { // This method will add two numbers and return result
         return num1 + num2;
     }
-
 }
