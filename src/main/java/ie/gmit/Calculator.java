bb992ed3f30a276bacc9cd46f9c05aace21aff23
PADRAIG O COSGORA - STUDENT <g00311302@gmit.ie>
2020-01-31 13:15:41 +0000

    IntelliJ update

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -1,3 +1,9 @@
+package ie.gmit;
+
 public class Calculator {
-    
+
+    public double add(double num1, double num2) { // This method will add two numbers
+        return num1 + num2;
+    }
+
 }
--- src/main/java/ie/gmit/CalculatorTest.java
+++ src/main/java/ie/gmit/CalculatorTest.java
@@ -1,4 +1,15 @@
 package ie.gmit;
 
 public class CalculatorTest {
+    public static void main(String[] args) {
+    // This class tests the Calculator class
+    Calculator calc = new Calculator();
+
+    double result = calc.add(20, 20);
+    if (result != 40) {
+        System.out.println("Not good");
+    } else {
+        System.out.println("Good");
+    }
+    }
 }
